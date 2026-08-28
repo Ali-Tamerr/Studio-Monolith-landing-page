b71288c925cd43cc42caeb91eb69c1c72563c9d2
Ali-Tamerr <extelx66@gmail.com>
2026-08-28 21:38:11 +0300

    ci: add GitHub Pages deployment workflow and configure static export settings

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const repoName = 'creative-agency-landing-page';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
+  trailingSlash: true,
   basePath: isProd ? `/${repoName}` : '',
   turbopack: {
     root: path.resolve(__dirname, '..'),
